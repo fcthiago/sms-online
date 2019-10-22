1a17f750daee3b1deff5169cce4dcfce8bc8fc4f
Thiago Ferreira <tfc1304@gmail.com>
2019-10-22 18:01:47 -0300

    Mudado mensagem da rota root

--- src/http/controllers/HttpController.js
+++ src/http/controllers/HttpController.js
@@ -15,7 +15,7 @@ module.exports = class HttpController {
             try {
                 response.json({
                     "status_code": 200,
-                    "message": "Bem vindo ao treinamento Teste!!"
+                    "message": "Bem vindo ao treinamento!!"
                 }).end()
             } catch (error) {
                 next(error)
