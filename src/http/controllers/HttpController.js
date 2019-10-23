6042ec960c9e0e5afd89897e5b0ceca4ab246837
Thiago Ferreira <tfc1304@gmail.com>
2019-10-23 12:09:10 -0300

    Teste do pipeline

--- src/http/controllers/HttpController.js
+++ src/http/controllers/HttpController.js
@@ -15,7 +15,7 @@ module.exports = class HttpController {
             try {
                 response.json({
                     "status_code": 200,
-                    "message": "último teste 123!!!!"
+                    "message": "último teste 1234!!!!"
                 }).end()
             } catch (error) {
                 next(error)
