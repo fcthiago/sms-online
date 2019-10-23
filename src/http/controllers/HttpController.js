86fec8e8269e038775a7646b7d21cdd36aea8355
Thiago Ferreira <tfc1304@gmail.com>
2019-10-23 09:37:52 -0300

    Teste do pipeline

--- src/http/controllers/HttpController.js
+++ src/http/controllers/HttpController.js
@@ -15,7 +15,7 @@ module.exports = class HttpController {
             try {
                 response.json({
                     "status_code": 200,
-                    "message": "Bem vindo ao teste de pipeline!!"
+                    "message": "Bem vindo ao teste de pipeline!!!!"
                 }).end()
             } catch (error) {
                 next(error)
